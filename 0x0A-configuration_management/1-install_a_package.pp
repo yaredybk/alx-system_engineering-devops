f503ad4a01c614de00ee95c0b04a4c4289259a26
Yared b <yb12ybk@gmail.com>
2024-04-28 19:49:21 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@
 # Install flask
 # Version must be 2.1.0
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
