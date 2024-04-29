5595a641a980ea1c322ec4c066588615b856cbc1
yaredybk <yb12ybk@gmail.com>
2024-04-28 18:20:22 -0700

    some updates

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Puppet to make changes to our configuration file
+# set up your client SSH configuration file to connect to a server
+# without typing a password.
+
+file_line{'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication n'
+}
+
+file_line{'Set identify file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school'
+}
