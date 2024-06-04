25583d9d07e22ac7c9c9bca62010993762619318
yaredybk <yb12ybk@gmail.com>
2024-06-04 16:28:49 -0700

    task-1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ import requests
 def number_of_subscribers(subreddit):
     """ a function that queries the Reddit API and
     returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about"
     headers = {
-            "User-Agent": "yaredb@alx"
+            "User-Agent": "yared_alx"
             }
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code >= 300:
         return 0
-    return res.json().get("data", {}).get("subscribers", 0)
+    tmp = res.json()
+    return tmp.get("data", {}).get("subscribers", 0)
