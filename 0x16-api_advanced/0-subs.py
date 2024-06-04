cfc0f57706ea77523513f31a70570b994cf30d41
yaredybk <yb12ybk@gmail.com>
2024-06-04 15:55:35 -0700

    task_0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a function that queries the Reddit API and
+returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ a function that queries the Reddit API and
+    returns the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "yaredb@alx"
+            }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code >= 300:
+        return 0
+    return res.json().get("data", {}).get("subscribers", 0)
